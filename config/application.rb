c3bc19b35bda3a896c6e66f4628e4566bb1d530f
2litrewt <buicnuwe0170@gmail.com>
2024-07-27 18:52:31 +0900

    test

--- config/application.rb
+++ config/application.rb
@@ -2,15 +2,15 @@ require_relative "boot"
 
 require "rails/all"
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module SampleApp
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+<<<<<<< HEAD
 
+=======
+>>>>>>> 8ee9939 (test)
     config.generators do |g|
       g.assets false
       g.helper false
