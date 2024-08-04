e0bf6efa6eea117ee396b4383f3e80246721876c
2litrewt <buicnuwe0170@gmail.com>
2024-08-04 15:41:34 +0900

    Step6: サインイン機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  get 'users/new'
-  get 'generate/controller'
-  get 'generate/tops'
-  get 'generate/new'
-  root :to => "tops#index"
+
+  root "tops#index"
   resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create]
+  resources :sessions, only: [:new]
 end
